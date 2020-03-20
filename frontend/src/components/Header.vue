26ae87b0c842c700371c0c99b3580216ddeb0c7c
Erhan2000 <erhan-kurnaz@hotmail.com>
2020-03-20 22:21:44 +0100

    started with vue

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <b-row>
+            <b-col sm="2">
+                <img src="../assets/logo.png" alt="kartcentrum" class="pull-left img-fluid" />
+            </b-col>
+            <b-col sm="10">
+                <h1>Kartcentrum <span class="text-danger"> MAX</span></h1>
+            </b-col>
+        </b-row>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'Header'
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+    <b-navbar variant="default">
+        <b-container fluid="true">
+            <div class="navbar-header">
+                <b-navbar-toggle target="_self">
+                    <span class="icon-bar" />
+                    <span class="icon-bar" />
+                    <span class="icon-bar" />
+                </b-navbar-toggle>
+                <b-icon-brightness-high />
+            </div>
+        </b-container>
+    </b-navbar>
+</template>
+
+<script>
+    export default {
+        name: 'Navbar',
+    }
+</script>
+
+<style scoped>
+
+</style>
